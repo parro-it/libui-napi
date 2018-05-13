1683e6322a311e0ee1e02ff7acb64f5043047850
Andrea Parodi <andrea@parro.it>
2018-05-13 23:27:21 +0200

    [WIP] Base control - JS

---
+++ js/ui-control.js
@@ -0,0 +1,48 @@
+const {ControlBase} = require('..');
+
+/**
+ * A simple button.
+ */
+class UiControl {
+	/**
+	 * Create a new UiControl object.
+	 * @return {UiControl}
+	 */
+	constructor(handle) {
+		this.handle = handle;
+	}
+
+	/**
+	 * Set or return whether the control is enabled.
+	 * @return {boolean}
+	 */
+	get enabled() {
+		return ControlBase.getEnabled(this.handle);
+	}
+
+	/**
+	 * Set or return whether the control is visible.
+	 * @return {boolean}
+	 */
+	get visible() {
+		return ControlBase.getVisible(this.handle);
+	}
+
+	/**
+	 * Set or return whether the control is a top level one.
+	 * @return {boolean}
+	 */
+	get toplevel() {
+		return ControlBase.getText(this.handle);
+	}
+
+	set visible(value) {
+		ControlBase.setVisible(this.handle, Boolean(value));
+	}
+
+	set enabled(value) {
+		ControlBase.setEnabled(this.handle, Boolean(value));
+	}
+}
+
+module.exports = {UiControl};
