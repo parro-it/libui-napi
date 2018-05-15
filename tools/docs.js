b3b937545b6f75692e3a7d3964a72944f0058026
Andrea Parodi <andrea@parro.it>
2018-05-15 22:27:09 +0200

    Made docs.js async

--- tools/docs.js
+++ tools/docs.js
@@ -1,9 +1,11 @@
 const jsdoc2md = require('jsdoc-to-markdown')
 const fs = require('fs')
 const path = require('path')
+const map = require('p-map')
+const toDotCase = require('to-dot-case');
 
-function render(mainClass, input, output) {
-	const data = jsdoc2md.getTemplateDataSync({files: input});
+async function render([mainClass, input]) {
+	const data = await jsdoc2md.getTemplateData({files: input});
 	const mainClassData =
 		data.sort((i1, i2) => i1.order - i2.order)
 			.filter(item => item.memberof === mainClass || item.id === mainClass)
@@ -11,50 +13,76 @@ function render(mainClass, input, output) {
 	if (mainClassData.length <= 1) {
 		throw new Error(`No data found for class ${mainClass} in files ${input}`);
 	}
-	const d = jsdoc2md.renderSync({data: mainClassData});
-	fs.writeFileSync(output, d)
+
+	const output = 'docs/' + toDotCase(mainClass.slice(2)).replace(/\./g, '-') + '.md';
+	const d = await jsdoc2md.render({data: mainClassData});
+	await fs.promises.writeFile(output, d);
+	console.log(`${mainClass} -> ${output}`);
 }
 
-render('UiArea', 'js/{area.js,ui-control.js}', 'docs/area.md');
-render('UiButton', 'js/{button.js,ui-control.js}', 'docs/button.md');
+function renderAll(...classes) {
+	map(classes, render, {concurrency: 5})
+		.then(() => console.log('Documentation generation completed.'))
+		.catch(err => console.error(err));
+}
 
-render('UiCheckbox', 'js/{checkbox.js,ui-control.js}', 'docs/checkbox.md');
-render('UiColorButton', 'js/{color-button.js,ui-control.js}', 'docs/color-button.md');
-render('UiCombobox', 'js/{combobox.js,ui-control.js}', 'docs/combobox.md');
-render('UiDatePicker', 'js/{date-picker.js,date-time-picker-base.js,ui-control.js}',
-	   'docs/date-picker.md');
-render('UiDateTimePicker',
-	   'js/{date-time-picker.js,date-time-picker-base.js,ui-control.js}',
-	   'docs/date-time-picker.md');
-render('UiEditableCombobox', 'js/{editable-combobox.js,ui-control.js}',
-	   'docs/editable-combobox.md');
-render('UiEntry', 'js/{entry.js,entry-base.js,ui-control.js}', 'docs/entry.md');
-render('UiForm', 'js/{form.js,ui-control.js}', 'docs/form.md');
-render('UiGrid', 'js/{grid.js,ui-control.js}', 'docs/grid.md');
-render('UiGroup', 'js/{group.js,ui-control.js}', 'docs/group.md');
-render('UiHorizontalBox', 'js/{horizontal-box.js,box.js,ui-control.js}',
-	   'docs/horizontal-box.md');
-render('UiHorizontalSeparator',
-	   'js/{horizontal-separator.js,separator-base.js,ui-control.js}',
-	   'docs/horizontal-separator.md');
-render('UiLabel', 'js/{label.js,ui-control.js}', 'docs/label.md');
-render('UiMenu', 'js/{menu.js,ui-control.js}', 'docs/menu.md');
-render('UiMultilineEntry', 'js/{multiline-entry.js,ui-control.js}',
-	   'docs/multiline-entry.md');
-render('UiPasswordEntry', 'js/{password-entry.js,entry-base.js,ui-control.js}',
-	   'docs/password-entry.md');
-render('UiProgressBar', 'js/{progress-bar.js,ui-control.js}', 'docs/progress-bar.md');
-render('UiRadioButtons', 'js/{radio-buttons.js,ui-control.js}', 'docs/radio-buttons.md');
-render('UiSearchEntry', 'js/{search-entry.js,entry-base.js,ui-control.js}',
-	   'docs/search-entry.md');
-render('UiSlider', 'js/{slider.js,ui-control.js}', 'docs/slider.md');
-render('UiSpinbox', 'js/{spinbox.js,ui-control.js}', 'docs/spinbox.md');
-render('UiTab', 'js/{tab.js,ui-control.js}', 'docs/tab.md');
-render('UiTimePicker', 'js/{time-picker.js,date-time-picker-base.js,ui-control.js}',
-	   'docs/time-picker.md');
-render('UiVerticalBox', 'js/{vertical-box.js,box.js,ui-control.js}',
-	   'docs/vertical-box.md');
-render('UiVerticalSeparator',
-	   'js/{vertical-separator.js,separator-base.js,ui-control.js}',
-	   'docs/vertical-separator.md');
-render('UiWindow', 'js/{window.js,ui-control.js}', 'docs/window.md');
+renderAll(
+	['UiArea', 'js/{area.js,ui-control.js}'],
+	['UiButton', 'js/{button.js,ui-control.js}'],
+	['UiCheckbox', 'js/{checkbox.js,ui-control.js}'],
+	['UiColorButton', 'js/{color-button.js,ui-control.js}'],
+	['UiCombobox', 'js/{combobox.js,ui-control.js}'],
+	[
+		'UiDatePicker',
+		'js/{date-picker.js,date-time-picker-base.js,ui-control.js}',
+	],
+	[
+		'UiDateTimePicker',
+		'js/{date-time-picker.js,date-time-picker-base.js,ui-control.js}',
+	],
+	[
+		'UiEditableCombobox',
+		'js/{editable-combobox.js,ui-control.js}',
+	],
+	['UiEntry', 'js/{entry.js,entry-base.js,ui-control.js}'],
+	['UiForm', 'js/{form.js,ui-control.js}'], ['UiGrid', 'js/{grid.js,ui-control.js}'],
+	['UiGroup', 'js/{group.js,ui-control.js}'],
+	[
+		'UiHorizontalBox',
+		'js/{horizontal-box.js,box.js,ui-control.js}',
+	],
+	[
+		'UiHorizontalSeparator',
+		'js/{horizontal-separator.js,separator-base.js,ui-control.js}',
+	],
+	['UiLabel', 'js/{label.js,ui-control.js}'], ['UiMenu', 'js/{menu.js,ui-control.js}'],
+	[
+		'UiMultilineEntry',
+		'js/{multiline-entry.js,ui-control.js}',
+	],
+	[
+		'UiPasswordEntry',
+		'js/{password-entry.js,entry-base.js,ui-control.js}',
+	],
+	['UiProgressBar', 'js/{progress-bar.js,ui-control.js}'],
+	['UiRadioButtons', 'js/{radio-buttons.js,ui-control.js}'],
+	[
+		'UiSearchEntry',
+		'js/{search-entry.js,entry-base.js,ui-control.js}',
+	],
+	['UiSlider', 'js/{slider.js,ui-control.js}'],
+	['UiSpinbox', 'js/{spinbox.js,ui-control.js}'],
+	['UiTab', 'js/{tab.js,ui-control.js}'],
+	[
+		'UiTimePicker',
+		'js/{time-picker.js,date-time-picker-base.js,ui-control.js}',
+	],
+	[
+		'UiVerticalBox',
+		'js/{vertical-box.js,box.js,ui-control.js}',
+	],
+	[
+		'UiVerticalSeparator',
+		'js/{vertical-separator.js,separator-base.js,ui-control.js}',
+	],
+	['UiWindow', 'js/{window.js,ui-control.js}']);
