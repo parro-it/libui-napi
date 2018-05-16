8552886a61c659cdfc2fd47cdb0be99db7d3091e
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-05-16 11:09:57 +0200

    Add getter/setter test

---
+++ tests/getter.js
@@ -0,0 +1,29 @@
+const test = require('tape');
+const {DrawBrush, Point, UiAreaKeyEvent} = require('..');
+
+test('getters and setters', t => {
+	t.plan(6);
+
+	const x = new DrawBrush();
+	t.ok(x.getType() === 0);
+	x.setType(1);
+	t.ok(x.getType() === 1);
+
+	const y = new Point(1, 2);
+	t.ok(y.getX() === 1);
+	y.setX(0);
+	t.ok(y.getX() === 0);
+
+	const z = new UiAreaKeyEvent(74, 0, 0, 0, true);
+	t.ok(z.getKey() == 'J');
+	t.ok((() => {
+		try {
+			z.setKey(40);
+		} catch (e) {
+			if (e instanceof TypeError) {
+				return true;
+			}
+		}
+		return false;
+	})());
+});
