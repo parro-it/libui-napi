fb88eae28e2e9592551bfe9dff15feb11ba65f08
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-05-31 23:03:21 +0200

    Fix new example test

--- tests/ui.js
+++ tests/ui.js
@@ -1,4 +1,6 @@
 const test = require('tape');
+const path = require('path');
+const execFileSync = require('child_process').execFileSync;
 
 let total = 0, passed = 0, failed = 0;
 
@@ -18,7 +20,11 @@ function tapLogger(type, file, err) {
 			failed++;
 			console.log(`not ok ${total} - ${file}`);
 			console.log('  ---');
-			console.log(`    message: Looks different, see ${err}`);
+			if (/_diff\.png$/.test(err)) {
+				console.log(`    message: Looks different, see ${err}`);
+			} else {
+				console.log(`    message: ${err}`);
+			}
 			console.log('  ...');
 			break;
 		case 'PASSED':
@@ -29,27 +35,27 @@ function tapLogger(type, file, err) {
 	}
 }
 
-process.chdir(__dirname);
+process.chdir(path.resolve(__dirname, '..'));
 const tester = require('screenshot-tester')(
-	{outDir: '_snapshots', accuracy: 500, logger: tapLogger});
-const testerRaw = require('screenshot-tester')(
-	{raw: 'node', outDir: '_snapshots', accuracy: 500, logger: tapLogger});
+	{outDir: 'tests/_snapshots', accuracy: 500, logger: tapLogger});
 
 test('ui', t => {
 	const start = test.getHarness()._results.count;
 	total = start;
 
-	tester('../example/area-adv.js', 'Area Advanced', {delay: 500, delta: 100})
-		.then(() => tester('../example/gallery/start.js', 'Control Gallery',
-						   {delay: 2500, delta: 100}))
-		.then(() => tester('../example/area-scrolling.js', 'Area window',
+	tester('example/area-adv.js', 'Area Advanced', {delay: 500, delta: 100})
+		.then(() => tester('example/area-scrolling.js', 'Area window',
 						   {delay: 500, delta: 100}))
-		.then(() => tester('../example/area.js', 'Area window', {delay: 500, delta: 100}))
-		.then(() => tester('../example/core-api.js', 'Test window'))
-		.then(() => tester('../example/forms.js', 'Forms window'))
-		.then(() => tester('../example/grid.js', 'Forms window'))
-		.then(() => tester('../example/node-pad.js', 'Node Pad'))
-		.then(() => tester('../example/text.js', 'textDrawArea Example'))
+		.then(() => tester('example/area.js', 'Area window', {delay: 500, delta: 100}))
+		.then(() => tester('example/core-api.js', 'Test window'))
+		.then(() => tester('example/forms.js', 'Forms window'))
+		.then(() => tester('example/grid.js', 'Forms window'))
+		.then(() => tester('example/node-pad.js', 'Node Pad'))
+		.then(() => tester('example/text.js', 'textDrawArea Example'))
+		.then(() => {
+			execFileSync('npm', ['run', 'build'], {cwd: path.join('example', 'gallery')});
+			return tester('example/gallery/dist/index.js', 'Control Gallery');
+		})
 		.then(() => tester.generateHTML())
 		.then(() => {
 			test.getHarness()._results.count += total - start;
