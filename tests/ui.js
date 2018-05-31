ee6a40a380f5c5c0f2a2d58343f1cf35969a906f
Andrea Parodi <andrea@parro.it>
2018-05-31 23:44:46 +0200

    add npm install & new package-lock

--- tests/ui.js
+++ tests/ui.js
@@ -53,6 +53,7 @@ test('ui', t => {
 		.then(() => tester('example/node-pad.js', 'Node Pad'))
 		.then(() => tester('example/text.js', 'textDrawArea Example'))
 		.then(() => {
+			execFileSync('npm', ['install'], {cwd: path.join('example', 'gallery')});
 			execFileSync('npm', ['run', 'build'], {cwd: path.join('example', 'gallery')});
 			return tester('example/gallery/dist/index.js', 'Control Gallery');
 		})
