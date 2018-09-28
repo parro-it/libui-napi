d0e8d95e88a0ba45c0996ea238aabd90bbbbd4b9
Andrea Parodi <andrea@parro.it>
2018-09-28 11:38:28 +0200

    advanced model [WIP]

--- example/tables.js
+++ example/tables.js
@@ -12,10 +12,121 @@ const win = new libui.UiWindow('Tables example', 800, 600, true);
 win.margined = true;
 const {ValueTypes} = libui.UiTableModel;
 
-let img;
+
+const always = val => () => val;
+const noop = () => undefined;
+
+function objectsTableModel(model) {
+	const numColumns = Object.keys(model).length;
+	const columnTypes = [];
+	const cellGetters = [];
+	const cellSetters = [];
+
+	for (const [key, column] of Object.entries(model)) {
+		const idx = columnTypes.length;
+		
+		if (column.type === String) {
+			columnTypes[idx] = ValueTypes.String;
+			cellGetters[idx] = (data, row) => {
+				debugger
+				return data[row][key]
+			};
+			cellSetters[idx] = (data, row, value) => {
+				debugger
+				data[row][key] = value
+			};
+			column.adder = tb => tb.appendTextColumn(column.header, idx, idx + 1, null);
+
+		} else if (column.type === 'Image') {
+			columnTypes[idx] = ValueTypes.Image;
+			debugger
+			if (column.value instanceof libui.UiImage) {
+				cellGetters[idx] = () => {
+					debugger
+					return column.value;
+				};
+			} else if (typeof column.value === 'function') {
+				cellGetters[idx] = column.value;
+			} else {
+				cellGetters[idx] = (data, row) => data[row][key];
+			}
+			column.adder = tb => tb.appendImageColumn(column.header, idx);
+
+			cellSetters[idx] = noop;
+		} 
+		
+		if (typeof column.editable === 'function') {
+			columnTypes[idx + 1] = ValueTypes.Int;
+			cellGetters[idx + 1] = (data, row) => column.editable(data[row]);
+			
+		} else {
+			columnTypes[idx + 1] = ValueTypes.Int;
+			cellGetters[idx + 1] = always(Number(Boolean(column.editable)));
+		}
+		cellSetters[idx + 1] = noop;
+		
+		if (column.header === undefined) {
+			column.header = key;
+		}
 
 
+	}
 
+	return {
+		fields: model,
+		addColumns(tb, ...columnNames) {
+			for (const columnName of columnNames) {
+				debugger
+				this.fields[columnName].adder(tb);
+			}
+		},
+		bind(data) {
+			return new libui.UiTableModel({
+				numColumns: () => numColumns,
+				columnType: column => columnTypes[column],
+				numRows: () => data.length,
+				cellValue: (row, column) => {
+					console.log({row, column})
+					const value = cellGetters[column](data, row)
+					console.log(value)
+					return value;
+				},
+				setCellValue: (row, column, value) => cellSetters[column](data, row, value)
+			});
+		}
+	};
+	
+}
+
+const model = objectsTableModel({
+	name: {
+		type: String,
+		editable: true,
+		header: 'Name'
+	},
+	surname: {
+		type: String
+	},
+	picture: {
+		type: 'Image',
+		value: () => {
+			debugger
+			return img
+		}
+	}
+});
+const data = [{
+	name: 'Andrea',
+	surname: 'Parodi'
+}, {
+	name: 'Giorgio',
+	surname: 'Parodi'
+}];
+
+
+const tb = new libui.UiTable(model.bind(data));
+model.addColumns(tb, 'name', 'surname', 'picture');
+/*
 const tb = new libui.UiTable(new libui.UiTableModel({
 	numColumns() {
 		return 3;
@@ -99,7 +210,7 @@ tb.appendCheckboxColumn('semver', 4, 41);
 tb.appendCheckboxTextColumn('version+semver', 4, 41, 1, 3, null);
 tb.appendButtonColumn('details', 5, 6);
 tb.appendImageColumn('img', 7);
-
+*/
 const vbox = new libui.UiVerticalBox();
 vbox.append(tb, true);
 win.setChild(vbox);
@@ -109,6 +220,7 @@ win.onClosing(() => {
 	libui.stopLoop();
 });
 
+let img = null;
 async function run() {
 	img = await libui.UiImage.loadFromPng(__dirname + '/lightning-orb.png');
 	win.show();	
